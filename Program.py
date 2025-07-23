2f25ff32f2ca82b4988d990c4354a5a6cfe9ba02
Sylwester Sowula <sylwester.sowula@gmail.com>
2025-07-24 00:10:43 +0200

    create function to print basic dataframe stats

--- Program.py
+++ Program.py
@@ -5,11 +5,16 @@ import numpy as np
 
 def main():
     df = data_fetch("TSLA")
+    basic_statistics(df)
 
 def data_fetch(ticker : str):
     data = yf.download(ticker,start='2025-06-01',end='2025-07-01')
     df=pd.DataFrame(data)
     return df
+
+def basic_statistics(df: pd.DataFrame):
+    stats = df.describe()
+    print(stats)
 
 if __name__ == "__main__":
     main()
