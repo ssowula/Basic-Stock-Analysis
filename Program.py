8c2689bc391c6313a77e3d3f9254b6a4c8aeb9b6
Sylwester Sowula <sylwester.sowula@gmail.com>
2025-07-24 00:12:13 +0200

    add open_close_plot function to visualize daily price movements

--- Program.py
+++ Program.py
@@ -6,6 +6,7 @@ import numpy as np
 def main():
     df = data_fetch("TSLA")
     basic_statistics(df)
+    open_close_plot(df)
 
 def data_fetch(ticker : str):
     data = yf.download(ticker,start='2025-06-01',end='2025-07-01')
@@ -15,6 +16,22 @@ def data_fetch(ticker : str):
 def basic_statistics(df: pd.DataFrame):
     stats = df.describe()
     print(stats)
+
+def open_close_plot(df: pd.DataFrame):
+    open_price = df['Open']
+    close_price = df['Close']
+    time = df.index
+
+    plt.figure(figsize=(20, 10))
+    plt.plot(time, open_price, label='Open', color='green')
+    plt.plot(time, close_price, label='Close', color='blue')
+    plt.title('Open and Close prices')
+    plt.xlabel('Date')
+    plt.ylabel('Price')
+    plt.legend()
+    plt.grid(True)
+    plt.tight_layout()
+    plt.show()
 
 if __name__ == "__main__":
     main()
