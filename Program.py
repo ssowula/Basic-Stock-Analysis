05470b6b1465efcf1d148f11922d959198632570
Sylwester Sowula <sylwester.sowula@gmail.com>
2025-07-24 16:14:41 +0200

    add VaR histogram plot with both historical and parametric methods

--- Program.py
+++ Program.py
@@ -10,8 +10,10 @@ def main():
     open_close_plot(df)
     calculate_daily_returns(df)
     calculate_cumulative_returns(df)
-    hist_var=historical_var(df, percentile=5.00)
-    param_var=parametric_var(df, confidence_level=0.95)
+    h_var=historical_var(df, percentile=5.00)
+    p_var=parametric_var(df, confidence_level=0.95)
+
+    var_plot(df,h_var,p_var)
 
     print(df)
 
@@ -50,6 +52,7 @@ def calculate_cumulative_returns(df: pd.DataFrame):
     df['Cumulative_returns'] = (1+df['Daily_returns']/100).cumprod()
     loss = 1 - df['Cumulative_returns'].iloc[-1]
     print(f"Cumulative loss in June: {round(loss * 100, 2)}%")
+
     plt.figure(figsize=(20, 10))
     plt.plot(df['Cumulative_returns'])
     plt.title('Cumulative returns')
@@ -60,7 +63,7 @@ def calculate_cumulative_returns(df: pd.DataFrame):
 
 def historical_var(df: pd.DataFrame, percentile : float):
     returns = df['Daily_returns'].dropna()
-    var_value = np.percentile(returns, percentile)
+    var_value = float(np.percentile(returns, percentile))
 
     confidence_level = 100 - percentile
 
@@ -74,6 +77,22 @@ def parametric_var(df:pd.DataFrame,confidence_level : float):
     var_value = mu + z*sigma
     print(f"Parametric Value at Risk (VaR) at {confidence_level*100:.0f}% confidence level: {var_value:.2f}%")
     return var_value
+
+def var_plot(df: pd.DataFrame, h_var : float,p_var :float):
+    daily_returns = df['Daily_returns']
+    plt.figure(figsize=(12, 6))
+    plt.hist(daily_returns, bins=40, color='skyblue', edgecolor='black', alpha=0.7)
+    plt.axvline(h_var, color='red', linestyle='--', linewidth=2, label=f'Historical VaR: {h_var:.2f}%')
+    plt.axvline(p_var, color='orange', linestyle='--', linewidth=2, label=f'Parametric VaR: {p_var:.2f}%')
+
+    plt.title('Histogram of Daily Returns with Historical and Parametric VaR')
+    plt.xlabel('Daily Return (%)')
+    plt.ylabel('Frequency')
+    plt.legend()
+    plt.grid(True)
+    plt.tight_layout()
+
+    plt.show()
 
 
 if __name__ == "__main__":
